e6f4ac40c1c492e21e5686f6afd19b392776d4cd
tanushkatariya312008-blip <tanushkatariya312008@gmail.com>
2026-09-19 04:32:36 +0530

    Refactor app.py to include port configuration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
@@ -101,4 +102,5 @@ def reset():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
